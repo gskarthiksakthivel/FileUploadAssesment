1c67f85d546d4d92c2ea98ce05fa05527b197f5e
Karthikeyan Sakthivel <gskarthiksakthivel@gmail.com>
2024-07-20 12:05:20 +0200

    Convert DTO into Record class for Data transfer

--- src/main/java/com/example/demo/dto/ExerciseDTO.java
+++ src/main/java/com/example/demo/dto/ExerciseDTO.java
@@ -1,19 +1,17 @@
 package com.example.demo.dto;
 
 
-import lombok.Data;
 
 import java.time.LocalDate;
 
 
-@Data
-public class ExerciseDTO {
-    private String code;
-    private String source;
-    private String codeListCode;
-    private String displayValue;
-    private String longDescription;
-    private LocalDate fromDate;
-    private LocalDate toDate;
-    private int sortingPriority;
-}
+public record ExerciseDTO(
+        String code,
+        String source,
+        String codeListCode,
+        String displayValue,
+        String longDescription,
+        LocalDate fromDate,
+        LocalDate toDate,
+        int sortingPriority
+) {}
